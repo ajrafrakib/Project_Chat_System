3c08b710afd54c636b524ca5d7a0618fc64b334f
Ajraf Rakib <md_ajraf.rakib@dfki.de>
2018-12-31 14:57:28 +0100

    a simple chat system

---
+++ handlers/messages.php
@@ -0,0 +1,41 @@
+<?php
+
+include('../config.php');
+
+switch ($_REQUEST['action']){
+
+	case "sendMessage":
+
+
+		session_start();
+		$query = $db->prepare("INSERT INTO messages SET user=?, message=?");
+		$run = $query->execute([$_SESSION['username'],$_REQUEST['message']]);
+
+		if ($run) {
+			echo 1;
+			exit;
+		}
+	break;	
+
+	case "getMessages":
+
+		$query = $db->prepare("SELECT * FROM messages");
+		$run = $query->execute();
+
+		$rs = $query->fetchALL(PDO::FETCH_OBJ);
+
+		$chat = '';
+		foreach ($rs as $message)
+		{
+
+			$chat .= '<div class = "single-message">
+						<strong>'.$message->user.':</strong> '.$message->message.'
+						<span>'.date('m-d-Y h:i a', strtotime($message->date)).'</span>
+					</div>';	
+		}
+		echo $chat;
+
+	break;
+}
+
+?>
